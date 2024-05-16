f9c0a897440f1f87374a3b0946ddd051f50916a6
Arenas <angelicacb6392@gmail.com>
2024-05-16 11:46:12 -0500

    Se hacen estructuracion del proyecto por carpetas y se vincula con la base de datos Supabase

---
+++ FrontEnd/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useContext, useState } from "react";
+
+
+
+export const Home = () => {
+
+  return (
+    <>
+    <div>
+        hola home
+    </div>
+
+    </>
+  );
+};
+
+export default Home;
