969ac324827b85e5cfa3089621505092e65dcead
Santiago <140932058+SantiagoMarin19@users.noreply.github.com>
2024-05-20 06:48:40 -0500

    Pagina Home y creacion de componentes

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './componentes/Navbar/Navbar';
+import Background from './componentes/Background/Background';
+
 
 function App() {
   return (
     <div>
-      <h1>Mi aplicación React</h1>
-      <p>Holamundo</p>
+      <Navbar></Navbar>
+      <Background></Background>
+   
     </div>
   );
 }
---
+++ FrontEnd/src/componentes/Background/Background.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Background.css';
+
+export const Background = () => {
+    return (
+
+        <div className="background">
+            <div className='Frase-background'> Renueva tu mirada, <br/>descubre tu resplandor</div>
+            
+            
+            
+            
+            <button className="btn btn-outline-success me-2 boton-reserva" type="button">Reservar Ahora</button>
+
+            
+        </div>
+        
+
+        
+
+
+
+    )
+
+}
+
+export default Background;
