64531c6a4173e93c516429f34d3be3057af39c77
Natalia <nataliaa95ss@gmail.com>
2024-05-24 23:03:16 -0500

    footerdenuevo
    se cambia toda la estructura del footer

--- FrontEnd/src/componentes/Footer/footer.jsx
+++ FrontEnd/src/componentes/Footer/footer.jsx
@@ -1,31 +1,28 @@
 import React from 'react';
-import './Foter.css';
-import 'FrontEnd/src/assets/images/Rectangle 201.png'
+import '../Footer/Footer.css';
+
+import { NavLink } from 'react-router-dom';
 
 export const Footer = () => {
     return (
 
-        <div className='all'>
-            <div className='secciones'>
-                <div className='contenido-secciones'>
-                <h4>Acerca de</h4>
-                <h4>Servicios</h4>
-                <h4>Citas</h4>
-            </div>
-             </div>
-             <div className='logo'>
-                <div className='contenido-logo'>
-                <img src={lol} />
-                </div>
-             </div>
-             
-              <div className='redes'>
-                <div className='contenido-redes'>
-                     <NavLink to="/"><i class='bx bxl-facebook'></i> </NavLink>
-                      <NavLink to="/"><i class='bx bxl-instagram-alt' ></i> </NavLink>
-                      </div>
-                       </div>
-                       </div>
+ <div className='contenedor'>
+    <div className='izquierda'>
+        <button>Acerca de</button>
+        <button>Servicios</button>
+        <button>Citas</button>
+    </div>
+    
+    <div className='centro'>
+        <h4>imagenlogo</h4>
+    </div>
+
+    <div className='derecha'> 
+    <h4>icono facebook</h4>
+    <h4>icono instagram</h4>
+    </div>
+
+ </div>
 
     );
 };
