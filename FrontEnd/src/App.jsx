f103bdd08e3b4a905841b836519856be220f5a50
Natalia Salguero <159500898+Nata-Sd@users.noreply.github.com>
2024-05-25 11:22:55 -0500

    finalizacion froned del footer
    se agregan colores, hover y se finaliza la estetica

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './componentes/Navbar/Navbar';
 import Background from './componentes/Background/Background';
+import  { Piedepagina } from './componentes/Footer/Footer';
 
 
 function App() {
   return (
-    <div>
-      <Navbar></Navbar>
-      <Background></Background>
    
-    </div>
+     <Piedepagina/>
+
+   
   );
 }
 
--- FrontEnd/src/componentes/Footer/footer.jsx
+++ FrontEnd/src/componentes/Footer/footer.jsx
@@ -1,33 +1,39 @@
 import React from 'react';
-import './Foter.css';
-import 'FrontEnd/src/assets/images/Rectangle 201.png'
+import '../Footer/Footer.css';
+import { NavLink } from 'react-router-dom';
 
-export const Footer = () => {
+
+export const Piedepagina = () => {
     return (
 
-        <div className='all'>
-            <div className='secciones'>
-                <div className='contenido-secciones'>
-                <h4>Acerca de</h4>
-                <h4>Servicios</h4>
-                <h4>Citas</h4>
+        <div className='footer'>
+          
+            <div className='contenedor'>
+            <div className='izquierda'>
+                <button>Acerca de </button>
+                <button>Servicios</button>
+                <button>Citas</button>
             </div>
-             </div>
-             <div className='logo'>
-                <div className='contenido-logo'>
-                <img src={lol} />
-                </div>
-             </div>
-             
-              <div className='redes'>
-                <div className='contenido-redes'>
-                     <NavLink to="/"><i class='bx bxl-facebook'></i> </NavLink>
-                      <NavLink to="/"><i class='bx bxl-instagram-alt' ></i> </NavLink>
-                      </div>
-                       </div>
-                       </div>
 
-    );
+            <div className='centro'>
+            imagen
+            </div>
+
+            <div className='derecha'>
+             
+            <i class='bx bxl-instagram iconos_grandes'></i>              
+            <i class='bx bxl-facebook-circle iconos_grandes '  ></i>
+            </div>
+            </div>
+
+        <div className='abajo'>
+            <p> © 2024 Copyright</p>
+            </div>
+            
+        </div>
+
+
+
+        );    
 };
 
-export default Footer;
