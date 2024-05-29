5b5f805b2c3f6c234cc2092c8db1de4efdf95e58
Santiago <140932058+SantiagoMarin19@users.noreply.github.com>
2024-05-29 10:34:06 -0500

    Se implementan nuevos componentes como promociones imagenes e iconos de redes sociales

--- FrontEnd/src/componentes/Background/Background.jsx
+++ FrontEnd/src/componentes/Background/Background.jsx
@@ -1,33 +1,43 @@
 import React from 'react';
 import './Background.css';
+import imagenrostro from "../../assets/images/fondo.png";
 
 export const Background = () => {
     return (
+
         <div className='conteinergeneral'>
-            <div className="background">
-                <div className='containergoblal'>
-                    <div className='Frase-background'>
-                        <h2>
-                            Se la mejor parte de ti
-                        </h2>
-                    </div>
-                    <div className='Frase-background'>
-                        <h1>
-                            CONSIGUE TU <br></br> ESTILO
+            <div className='iconos'>
+                <div className="background">
+                    <div className='containergoblal'>
+                        <div className='Frase-background'>
+                            <h2>
+                                Se la mejor parte de ti
+                            </h2>
+                        </div>
+                        <div className='Frase-background'>
+                            <h1>
+                                CONSIGUE TU <br></br> ESTILO
 
-                        </h1>
-                    </div>
-                    <div className='Frase-background' >
-                        <p> ¡Haz de tu peinado una parte importante para la expresión de tu identidad! <br></br>
-                            ¡Nuestros peluqueros autorizados se asegurarán de que obtengas el estilo exacto que deseas!</p>
+                            </h1>
+                        </div>
+                        <div className='Frase-background' >
+                            <p> ¡Haz de tu peinado una parte importante para la expresión de tu identidad! <br></br>
+                                ¡Nuestros peluqueros autorizados se asegurarán de que obtengas el estilo exacto que deseas!</p>
 
+                        </div>
+                        <button type="button" class="btn btn-primary boton">AGENDAR  AHORA</button>
                     </div>
-                </div>
-           
+
+
                     <div className='foto'>
-                        <img src="../" alt="" />
+                        <img className="img-rostro" src={imagenrostro} />
                     </div>
-      
+
+                    <i class='bx bxl-instagram-alt icono_grande'> </i>
+                    <i class='bx bxl-facebook-circle icono_grande' > </i>
+                </div>
+
+
 
 
 
@@ -44,6 +54,7 @@ export const Background = () => {
 
                         Renueva tu mirada, <br />descubre tu resplandor
 
+
                     </div>
                     <div className='parrafo' >
                         <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta suscipit omnis,
@@ -54,6 +65,7 @@ export const Background = () => {
                 </div>
 
             </div>
+
 
 
 
---
+++ FrontEnd/src/componentes/Promociones/Promociones.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Promociones.css";
+
+
+
+export const Promociones = () => {
+    return (
+
+        <div className='Promociones'>
+          COMBO DEL MES :CEJAS MAS LIFTING DE  PESTAÑA
+        </div>
+    );
+}
+
+export default Promociones;
