6f52722f0d9a510f6d5f99e215d5782d4128f72e
David <149876832+D4vz4r@users.noreply.github.com>
2024-06-14 11:42:22 -0500

    Mejoras

--- FrontEnd/src/componentes/CompoServicios1/CompServicios1.jsx
+++ FrontEnd/src/componentes/CompoServicios1/CompServicios1.jsx
@@ -9,26 +9,26 @@ import imagencejas from "../../assets/images/cejas.png";
 
 export const CompServicios1 = () => {
     return (
-<div className='cejas'>
-        <div className='apartadoCejas'>
-            <img className="cej1" src={cejas1} />
-            <p>Cejas</p>
-        </div>
-        <div className='mitadeso'>
-        <div className='apartado1'>
-            <img className="cej2" src={cej} />
-            <p>Diseño-Depilación y Sombreado</p>
-        </div>
-        <div className='apartado2'>
-            <img className="completa" src={imagencompleta} />
-            <p>Diseño Depilación en Henna</p> 
-        </div>
-        <div className='apartado3'>
-            <img className="cej3" src={imagencejas} />
-            <p>Laminado de cejas</p>
-        </div>
-        </div>
-</div>
+        
+        <div className='cejas'>
+            <div className='apartadoCejas'>
+                <p>Cejas</p>
+            </div>
+
+            <div className='mitadeso'>
+                <div className='apartado1'>
+                    <p>Diseño Depilación en Henna</p>
+                    </div>
+                </div>
+                
+                <div className='apartado2'>
+                    <p>Diseño-Depilación y Sombreado</p>
+                </div>
+
+                <div className='apartado3'>
+                    <p>Laminado de cejas</p>
+                </div>
+            </div>
     );
 }
 
