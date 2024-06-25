4be263c610fe3b4d3c6ef163d9b08a35914f6a84
David Ochoa <149876832+D4vz4r@users.noreply.github.com>
2024-06-25 18:21:29 -0500

    Mejoras

--- FrontEnd/src/componentes/CompoServicios/CompServicios.jsx
+++ FrontEnd/src/componentes/CompoServicios/CompServicios.jsx
@@ -24,30 +24,85 @@ export const CompServicios = () => {
                 <img className="img-cej" src={cej} />
                 <h5>Diseño-Depilación y sombreado</h5>
                 <h5><b>$49.00</b></h5>
+                <button>Reservar</button>
                 </div>
                 <div className='concej1'>
                 <img className="img-cej" src={cej} />
                 <h5>Diseño-Depilación y sombreado</h5>
                 <h5><b>$49.00</b></h5>
+                <button>Reservar</button>
                 </div>
                 <div className='concej1'>
                 <img className="img-cej" src={cej} />
                 <h5>Diseño-Depilación y sombreado</h5>
                 <h5><b>$49.00</b></h5>
+                <button>Reservar</button>
                 </div>
                 </div>
 
                 <h3> Servicio Pestaña</h3>
                 <div className='serpes'>
-                <div className='concej1'>
-                <img className="img-cej" src={cej} />
+                <div className='conpes1'>
+                <img className="img-pes" src={cej} />
                 <h5>Diseño-Depilación y sombreado</h5>
                 <h5><b>$49.00</b></h5>
+                <button>Reservar</button>
                 </div>
-                <div className='concej1'>
-                <img className="img-cej" src={cej} />
+                <div className='conpes1'>
+                <img className="img-pes" src={cej} />
                 <h5>Diseño-Depilación y sombreado</h5>
                 <h5><b>$49.00</b></h5>
+                <button>Reservar</button>
+                </div>
+                </div>
+
+                <h3> Servicio de Micropigmentación</h3>
+                <div className='sermic'>
+                <div className='conmic1'>
+                <img className="img-mic" src={cej} />
+                <h5>Micropigmentacion Hair Stroke </h5>
+                <h5><b>$49.00</b></h5>
+                <button>Reservar</button>
+                </div>
+                <div className='conmic1'>
+                <img className="img-mic" src={cej} />
+                <h5>Diseño Depilación en Henna </h5>
+                <h5><b>$49.00</b></h5>
+                <button>Reservar</button>
+                </div>
+                <div className='conmic1'>
+                <img className="img-mic" src={cej} />
+                <h5>Micropigmentacion de Cejas </h5>
+                <h5><b>$49.00</b></h5>
+                <button>Reservar</button>
+                </div>
+                </div>
+
+                <h3> Combos</h3>
+                <div className='sercom'>
+                <div className='concom1'>
+                <img className="img-com" src={cej} />
+                <h5>Diseño depilacion en henna + lifting de pestañas </h5>
+                <h5><b>$49.00</b></h5>
+                <button>Reservar</button>
+                </div>
+                <div className='concom1'>
+                <img className="img-com" src={cej} />
+                <h5>laminacion de cejas+ extensiones de pestañas </h5>
+                <h5><b>$49.00</b></h5>
+                <button>Reservar</button>
+                </div>
+                <div className='concom1'>
+                <img className="img-com" src={cej} />
+                <h5>Diseño depilacion y sombreado + lifting de pestañas </h5>
+                <h5><b>$49.00</b></h5>
+                <button>Reservar</button>
+                </div>
+                <div className='concom1'>
+                <img className="img-com" src={cej} />
+                <h5>laminacion de cejas+ Lifting de pestañas </h5>
+                <h5><b>$49.00</b></h5>
+                <button>Reservar</button>
                 </div>
                 </div>
 
