e1ae7abc25ebbc8022a6310a32ffedb247cf59e9
Natalia Salguero <159500898+Nata-Sd@users.noreply.github.com>
2024-06-26 09:21:10 -0500

    app

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CompServicios from './pages/Servicios';
+import {Piedepagina} from './componentes/Footer/footer';
+import {  Pestañas} from "./componentes/Servicio_detalle/serviciopestañas";
+import {Cejas} from "./componentes/Servicio_detalle/Serviciocejas"
+import { Micropigmentacion } from "./componentes/Servicio_detalle/serviciomicropigmentacion";
 
 
 
