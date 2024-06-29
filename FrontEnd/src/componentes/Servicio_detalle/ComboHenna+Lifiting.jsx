4b05d337c80b12b37689b4a959d82d4360382ec2
Natalia Salguero <159500898+Nata-Sd@users.noreply.github.com>
2024-06-29 11:06:50 -0500

    cambios en el combo

--- FrontEnd/src/componentes/Servicio_detalle/ComboHenna+Lifiting.jsx
+++ FrontEnd/src/componentes/Servicio_detalle/ComboHenna+Lifiting.jsx
@@ -43,15 +43,20 @@ export const ComboHyLifting = () => {
                     </div>
                     </div> 
 
-                    <div className='contenido_botones'>
-                    <div className='informacion'>
-                        <p>120 Minutos</p>
-                        <p>$120.000</p>
-                        </div>
-                        
-                        <div className='botonera'>
-                            <button id='boton'>Reservar Ahora</button>
+                    <div className='content_boton'>     
+                    <div className='botones'>
+                                <button id='boton'>Reservar Ahora</button>
                             </div>
+                                <div className='botontyp'>  
+                                    <div className='tiempo'>
+                                        <h4>Tiempo</h4>
+                                        <p>60 Minutos</p>
+                                    </div>
+                                    <div className='precio'>
+                                        <h4>Precio</h4>
+                                        <p>$120.000</p>
+                                    </div>
+                                </div>
                             </div>
         </div>
     
