117d1ded3095ae11cbcab55a4b22947cd48f6324
Natalia Salguero <159500898+Nata-Sd@users.noreply.github.com>
2024-08-03 08:53:40 -0500

    Se empieza configuracion del componente de citas, se añade la logica de personal

--- FrontEnd/src/componentes/Dahsboard/Citas.jsx
+++ FrontEnd/src/componentes/Dahsboard/Citas.jsx
@@ -1,27 +1,51 @@
-import React from 'react';
-import "./Navbar.css";
+import React, { useState, useEffect } from 'react';
+import './Navbar.css';
 import { NavLink } from 'react-router-dom'; 
 
 export const AgendarCitaAdmin = () => {
+    const [profesionales, setProfesionales] = useState([]);
+    const [selectedProfesional, setSelectedProfesional] = useState('');
+
+    useEffect(() => {
+        // Función para obtener los datos de los profesionales
+        const fetchProfesionales = async () => {
+            try {
+                const response = await axios.get('https://api.example.com/profesionales'); // Cambia la URL a tu endpoint
+                setProfesionales(response.data);
+            } catch (error) {
+                console.error('Error al obtener los profesionales:', error);
+            }
+        };
+
+        fetchProfesionales();
+    }, []);
+
+    const handleChange = (event) => {
+        setSelectedProfesional(event.target.value);
+    };
+
     return (
         <div className='contenedor_AG_ADMIN'>
             <div className='Contenido_AG_ADMIN'>
                 <div className='Lista_profesionales_AG_ADMIN'>
                     <div className='Titulo_profesionales__AG_ADMIN'>
-                    <h4>Profesionales</h4>
+                        <h4>Profesionales</h4>
                     </div>
+                    
                     <div className='Lista_Profesionales_AG_ADMIN'>
-                    <ul>
-                        <li>Natalia Salazar </li>
-                        <li>Derian Marcarthur </li>
-                        <li>Freddy Sierra Silva </li>
-                    </ul>
+                        <select value={selectedProfesional} onChange={handleChange}>
+                            <option value="" disabled>Selecciona un profesional</option>
+                            {profesionales.map(profesional => (
+                                <option key={profesional.id} value={profesional.id}>
+                                    {profesional.nombre}
+                                </option>
+                            ))}
+                        </select>
                     </div>
                     
                 </div>
             </div>
         </div>
-
     );
 }
 
--- FrontEnd/src/pages/Diagramas.jsx
+++ FrontEnd/src/pages/Diagramas.jsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 export function Diagramas() {
-  return (<Container>
-<h1>Diagramas</h1>
-  </Container>);
+  return (
+  <Container>
+    <h1>Diagramas</h1>
+    </Container>);
 }
+
 const Container =styled.div`
    height:100vh;
 `
