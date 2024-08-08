5fb573a674725bbb8fe699034ad8b8821748ef02
Natalia Salguero <159500898+Nata-Sd@users.noreply.github.com>
2024-08-08 08:27:05 -0500

    Se termina seccion de Froned para cliente

--- FrontEnd/src/componentes/Footer/footer.jsx
+++ FrontEnd/src/componentes/Footer/footer.jsx
@@ -2,42 +2,43 @@ import React from 'react';
 import '../Footer/Footer.css';
 import { NavLink } from 'react-router-dom';
 
-
 export const Piedepagina = () => {
     return (
-
-        <div className='footer'>
-            <div className='contenedorup'>
-                <div className='encabezadofooter'></div>
-                <h3> <i className='bx bxl-instagram iconos_grandes'></i>
-                    <i className='bx bxl-facebook-circle iconos_grandes'></i></h3>
+        <footer className='footer'>
+            <div className='footer-top'>
+                <div className='social-media'>
+                    <a href='https://instagram.com' target='_blank' rel='noopener noreferrer'>
+                        <i className='bx bxl-instagram icon'></i>
+                    </a>
+                    <a href='https://facebook.com' target='_blank' rel='noopener noreferrer'>
+                        <i className='bx bxl-facebook-circle icon'></i>
+                    </a>
+                </div>
             </div>
 
-            <div className='contenedorif'>
-                <div className='izquierda'>
+            <div className='footer-content'>
+                <div className='footer-section'>
                     <h3>Explorar</h3>
-                
-                <NavLink to="/"><p>Inicio </p></NavLink>
-                <NavLink to="/ServicioCejas"> <p>Acerca De</p></NavLink>
-                <NavLink to="/servicios"> <p>Servicios</p></NavLink>
+                    <NavLink to="/"><p>Inicio</p></NavLink>
+                    <NavLink to="/ServicioCejas"><p>Acerca De</p></NavLink>
+                    <NavLink to="/servicios"><p>Servicios</p></NavLink>
                 </div>
 
-                <div className='centro'>
+                <div className='footer-section'>
                     <h3>Utilidades</h3>
                     <NavLink to="/Politicas"><p>Terminos y Condiciones</p></NavLink>
                 </div>
 
-                <div className='derecha'>
+                <div className='footer-section'>
                     <h3>Contacto</h3>
-                    <p><b>Dirección:</b>Calle 33b #7-53.</p>
-                    <p><b>Gmail:</b>nataliasalazarartist@gmail.com</p>
+                    <p><b>Dirección:</b> Calle 33b #7-53.</p>
+                    <p><b>Gmail:</b> nataliasalazarartist@gmail.com</p>
                 </div>
             </div>
 
-            <div className='abajo'>
-                <p> Copyright 2024, NataliaSalasar | Todos los derechos reservados</p>
+            <div className='footer-bottom'>
+                <p>Copyright 2024, NataliaSalasar | Todos los derechos reservados</p>
             </div>
-        </div>
-
+        </footer>
     );
 };
