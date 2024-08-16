e7f3a02fb60ad5fc49ca4cfae3c33ac11a937291
David Ochoa <149876832+D4vz4r@users.noreply.github.com>
2024-08-16 15:01:55 -0500

    Inicio de sesion
    Cuando el usuario inicie sesion se quita el boton iniciar sesion y se vuelve cerrar sesion

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,63 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Navbar } from "../componentes/Navbar/Navbar";
 import { Background } from "../componentes/Background/Background";
 import { Promociones } from "../componentes/Promociones/Promociones";
 import { useNavigate } from "react-router-dom";
 import { Piedepagina } from "../componentes/Footer/footer";
 
-export const Home = () => {
+export const Home = ({ token }) => {
   const navigate = useNavigate();
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    // Recupera el token del sessionStorage
-    const storedToken = sessionStorage.getItem('sb-bxluhldahxrqbukrqcdy-auth-token');
-    if (storedToken) {
-      try {
-        const parsedToken = JSON.parse(storedToken);
-        if (parsedToken && parsedToken.user && parsedToken.user.user_metadata) {
-          setUser(parsedToken.user.user_metadata.full_name);
-        } else {
-          setUser(null);
-        }
-      } catch (error) {
-        console.error('Error parsing token:', error);
-        setUser(null); // En caso de error, asegurarse de que el estado no contenga datos corruptos
-      }
-    } else {
-      setUser(null); // Si no hay token, asegurar que el estado esté limpio
-    }
-  }, []);
 
   function handleLogout() {
-    // Eliminar el token y otros datos del localStorage y sessionStorage
-    localStorage.removeItem('sb-bxluhldahxrqbukrqcdy-auth-token'); // Eliminar del localStorage
-    sessionStorage.removeItem('sb-bxluhldahxrqbukrqcdy-auth-token'); // Eliminar del sessionStorage
-    sessionStorage.removeItem('userDetails'); // Eliminar otros datos si es necesario
-
-    // Actualizar el estado local
-    setUser(null);
-
-    // Redirigir a la página de inicio de sesión
-    window.location.reload
-
-    // Verificar que los datos hayan sido eliminados
-    console.log('Local Storage:', localStorage.getItem('sb-bxluhldahxrqbukrqcdy-auth-token'));
-    console.log('Session Storage:', sessionStorage.getItem('sb-bxluhldahxrqbukrqcdy-auth-token'));
+    sessionStorage.removeItem('token');
+    window.location.reload(); // Recarga la página después de cerrar sesión
   }
 
   return (
     <>
       <Promociones />
-      <Navbar />
+      <Navbar token={token} handleLogout={handleLogout} />
       <div className="user">
         <i className='bx bxs-user-circle'>
-          {user}
+          {token && token.user.user_metadata.full_name}
         </i>
       </div>
-      <button onClick={handleLogout}>
-        Cerrar Sesión
-      </button>
       <Background />
       <Piedepagina />
     </>
