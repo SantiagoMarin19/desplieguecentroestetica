244c82f7603e8b9c9ca08b8a22c0e30c16b6de2f
Nata-Sd <159500898+Nata-Sd@users.noreply.github.com>
2024-09-07 06:32:08 -0500

    Componentes innecesarios

--- FrontEnd/src/componentes/PagesAdmin/routers/Route.jsx
+++ FrontEnd/src/componentes/PagesAdmin/routers/Route.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { PersonalAdmin } from "../PersonalAdmin";
 import  Citas from '../CitasAdmin';
-import { DiagramasAdmin } from "../DiagramasAdmin";
 import { ServiciosAdmin } from "../ServiciosAdmin";
 
 export function MyRoutes() {
@@ -11,7 +10,6 @@ export function MyRoutes() {
    
      
       <Routes>
-        <Route path="/DiagramasAdmin" element={<DiagramasAdmin />} /> 
         <Route path="/PersonalAdmin" element={<PersonalAdmin />} />
         <Route path="/AdminCitas" element={< Citas/>}/>
         <Route path="/ServiciosAdmin" element={<ServiciosAdmin/>}/>
